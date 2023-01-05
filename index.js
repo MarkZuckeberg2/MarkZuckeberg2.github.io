daa8ecb4c6b705432e5cbb069bc755ad0ba27983
MarkZuckeberg2 <122019026+MarkZuckeberg2@users.noreply.github.com>
2023-01-05 14:21:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 
+// send message to server
+function sendMessage() {
+  // get message text
+  var message = document.getElementById('message-input').value;
+
+  // send message to server
+  // ...
+
+  // clear message input
+  document.getElementById('message-input').value = '';
+}
+
+// retrieve and display messages from server
+function getMessages() {
+  // retrieve messages from server
+  // ...
+
+  // display messages
+  // ...
+}
+
+// listen for form submission
+document.getElementById('message-form').onsubmit = function(event) {
+  event.preventDefault();
+  sendMessage();
+};
+
+// get initial set of messages
+getMessages();
+
+// update messages every 10 seconds
+setInterval(getMessages, 10000);
