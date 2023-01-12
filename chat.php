5a45b0097b4621e59471d59d2ae808895b8c481d
MarkZuckeberg2 <122019026+MarkZuckeberg2@users.noreply.github.com>
2023-01-12 10:23:54 +0000

    Update chat.php

--- chat.php
+++ chat.php
@@ -1,21 +1,22 @@
 <?php
-// retrieve messages from database
+// Connect to database
 $db = new PDO('mysql:host=localhost;dbname=chat', 'username', 'password');
-$stmt = $db->query('SELECT * FROM messages ORDER BY id DESC');
-$messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// handle new message
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $message = json_decode(file_get_contents('php://input'), true);
-  $stmt = $db->prepare('INSERT INTO messages (text) VALUES (:text)');
-  $stmt->execute(['text' => $message['message']]);
-  exit();
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // retrieve messages from database
+    $stmt = $db->query('SELECT * FROM messages ORDER BY id DESC');
+    $messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    // format messages for display
+    $output = '';
+    foreach ($messages as $message) {
+        $output .= '<div>' . $message['text'] . '</div>';
+    }
+    echo $output;
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // handle new message
+    $message = json_decode(file_get_contents('php://input'), true);
+    $stmt = $db->prepare('INSERT INTO messages (text) VALUES (:text)');
+    $stmt->execute(['text' => $message['message']]);
+    exit();
 }
-
-// format messages for display
-$output = '';
-foreach ($messages as $message) {
-  $output .= '<div>' . $message['text'] . '</div>';
-}
-
-echo $output;
