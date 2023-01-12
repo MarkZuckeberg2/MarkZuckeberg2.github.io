725a7be7064824614d00c15540a354ec2f1993ce
MarkZuckeberg2 <122019026+MarkZuckeberg2@users.noreply.github.com>
2023-01-12 10:06:43 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,92 +1,11 @@
 <?php
-
-// connect to database
-$host = "localhost";
-$user = "username";
-$password = "password";
-$dbname = "database_name";
-
-$conn = mysqli_connect($host, $user, $password, $dbname);
-
-// check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-// retrieve messages
-$sql = "SELECT message FROM messages";
-$result = mysqli_query($conn, $sql);
-
-// create array to hold messages
-$messages = array();
-
-// add each message to array
-while ($row = mysqli_fetch_assoc($result)) {
-  $messages[] = $row['message'];
-}
-
-// return messages as JSON object
-header('Content-Type: application/json');
-echo json_encode($messages);
-
-// close database connection
-mysqli_close($conn);
-
+    // Connect to the database
+    $conn = mysqli_connect("host", "username", "password", "database");
+    // Get the post data from the request
+    $username = $_POST['username'];
+    $topic = $_POST['topic'];
+    $message = $_POST['message'];
+    // Insert the post data into the database
+    $query = "INSERT INTO posts (username, topic, message) VALUES ('$username', '$topic', '$message')";
+    $result = mysqli_query($conn, $query);
 ?>
-
-<?php
-
-// connect to database
-$host = "localhost";
-$user = "username";
-$password = "password";
-$dbname = "database_name";
-
-$conn = mysqli_connect($host, $user, $password, $dbname);
-
-// check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-// retrieve messages
-$sql = "SELECT sender, message FROM messages";
-$result = mysqli_query($conn, $sql);
-
-// create array to hold messages
-$messages = array();
-
-// add each message to array
-while ($row = mysqli_fetch_assoc($result)) {
-  $messages[] = array(
-    'sender' => $row['sender'],
-    'message' => $row['message']
-  );
-}
-
-// return messages as JSON object
-header('Content-Type: application/json');
-echo json_encode($messages);
-
-// close database connection
-mysqli_close($conn);
-
-?>
-
-// retrieve and display messages from server
-function getMessages() {
-  // retrieve messages from server
-  fetch('/messages')
-    .then(response => response.json())
-    .then(messages => {
-      // clear existing messages
-      document.getElementById('messages').innerHTML = '';
-
-      // display messages
-      messages.forEach(message => {
-        var messageElem = document.createElement('div');
-        messageElem.innerHTML = `<b>${message.sender}:</b> ${message.message}`;
-        document.getElementById('messages').appendChild(messageElem);
-      });
-    });
-}
